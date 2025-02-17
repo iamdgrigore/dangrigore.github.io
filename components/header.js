02b2bad2f36db71ac9de4cc2563417910e28751f
iamdgrigore <iamdgrigore@gmail.com>
2025-02-17 17:42:30 -0500

    Update header.js

--- components/header.js
+++ components/header.js
@@ -3,6 +3,10 @@ const headerTemplate = document.createElement('template');
 headerTemplate.innerHTML = `
   <style>
     nav {
+      position: fixed;
+      top: 0;
+      left: 0;
+      width: 100%;
       height: 40px;
       display: flex;
       align-items: center;
