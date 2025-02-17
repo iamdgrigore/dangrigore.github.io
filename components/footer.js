ec0e6ca6a58ef3014aaf6679e23b6ee606f8dc33
iamdgrigore <iamdgrigore@gmail.com>
2025-02-17 17:43:09 -0500

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -8,6 +8,10 @@ class Footer extends HTMLElement {
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
       <style>
         footer {
+          position: fixed;
+          bottom: 0;
+          left: 0;
+          width: 100%;
           height: 60px;
           padding: 0 10px;
           list-style: none;
